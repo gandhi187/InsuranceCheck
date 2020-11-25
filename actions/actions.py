fb95359500ee02e34e2a865ecd7295792971f0b9
dash <david.sharma_extern@ppi.de>
2020-11-25 19:09:49 +0100

    changed continent information

--- actions/actions.py
+++ actions/actions.py
@@ -137,7 +137,7 @@ class InsuranceCheck(FormAction):
 
         else:
             dispatcher.utter_message(
-                destination + " befindet sich in " + continent)
+                destination + " befindet sich in  " + continent)
             dispatcher.utter_message("In " + value + " gibt es derzeit " + str(activeCases) +
             " aktive Corona Fälle. \n Seit Ausbruch gab es " + str(deaths) + " Todesfälle.")
 
